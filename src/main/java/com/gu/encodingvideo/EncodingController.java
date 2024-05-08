77b823ac9f7d5e4a9406bb4000194dd22ddf9f8b
rnskqkd <rnskqkd@naver.com>
2024-05-08 09:21:40 +0900

    스레드 제어

--- src/main/java/com/gu/encodingvideo/EncodingController.java
+++ src/main/java/com/gu/encodingvideo/EncodingController.java
@@ -29,24 +29,19 @@ public class EncodingController {
     private final EncodingService encodingService;
 
     @PostMapping(value = "/encode", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    @Operation(summary = "비디오 인코딩", description = "MP4 비디오를 WebM 형식으로 인코딩합니다.")
-    public ResponseEntity<ApiResponse<String>> encodeVideo(@RequestParam("file") MultipartFile file) {
-        try {
-            CompletableFuture<String> future = encodingService.convertMp4ToWebmFile(file);
+    @Operation(summary = "비디오 인코딩", description = "MP4 비디오를 WebM 형식으로 인코딩")
+    public ResponseEntity<ApiResponse<String>> encodeVideo(@RequestParam("file") MultipartFile file) throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = encodingService.convertMp4ToWebmFile(file);
 
-            String result = future.get();
+        String result = future.get();
 
-            return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK, result);
-        } catch (ExecutionException | InterruptedException e) {
-            log.error(e.getMessage(), e);
-            throw new BadRequestException(ErrorCode.INVALID_FILE);
-        }
+        return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK, result);
     }
 
-    @GetMapping("/status/{id}")
-    @Operation(summary = "인코딩 상태 조회", description = "비디오 인코딩 상태를 조회합니다.")
-    public ResponseEntity<ApiResponse<String>> getEncodingStatus(@PathVariable("id") String id) {
-        String status = "인코딩 중";
-        return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK, id);
+    @DeleteMapping("/stop")
+    @Operation(summary = "인코딩 중지", description = "비디오 인코딩 중지")
+    public ResponseEntity<ApiResponse<Void>> stopAllProcesses() {
+        encodingService.stopAllProcesses();
+        return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK);
     }
 }
--- src/main/java/com/gu/encodingvideo/EncodingService.java
+++ src/main/java/com/gu/encodingvideo/EncodingService.java
@@ -6,8 +6,10 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +21,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,67 +40,78 @@ public class EncodingService {
     public boolean IS_WINDOWS = OS.contains("windows");
 
     @Value("${app-config.file-upload.base-path}")
-    String basePath;
+    private String basePath;
 
     @Value("${app-config.file-upload.encoding-path}")
-    String encodingPath;
+    private String encodingPath;
 
-        /**
-         * 변환
-         */
-        @Async
-        @Transactional
-        public CompletableFuture<String> convertMp4ToWebmFile(MultipartFile file) {
-            String[] defaultCmd = new String[]{
+    private final SimpMessagingTemplate messagingTemplate;
+
+    /* 현재 실행 중인 프로세스 목록 */
+    private final List<Process> processes = new CopyOnWriteArrayList<>();
+
+    /* 프로세스 진행 상태 */
+    private Boolean status = false;
+
+    /**
+     * 변환
+     */
+    @Async
+    @Transactional
+    public CompletableFuture<String> convertMp4ToWebmFile(MultipartFile file) {
+        stopAllProcesses();
+
+        if (status)
+            throw new BadRequestException(ErrorCode.EXCEED_MAX_UPLOAD_COUNT);
+
+        ProcessBuilder builder = new ProcessBuilder();
+        List<String> videoExtensions = Arrays.asList("mp4", "avi", "mov", "mpg", "wmv", "mpeg", "webm");
+        String inputFileString = basePath + encodingPath + File.separator + file.getOriginalFilename();
+        Path encodingFolderPath = Path.of(basePath, encodingPath);
+
+        if (videoExtensions.stream().noneMatch(file.getOriginalFilename().toLowerCase()::endsWith)) {
+            throw new BadRequestException(ErrorCode.INVALID_FILE);
+        }
+
+        try (BufferedInputStream input = new BufferedInputStream(file.getInputStream());
+                BufferedOutputStream output = new BufferedOutputStream(Files.newOutputStream(Path.of(inputFileString)))
+        ) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            log.error("Failed to save the file: {}", e.getMessage());
+        }
+
+        /* .webm 확장자일때 변환 생략 */
+        if (!file.getOriginalFilename().toLowerCase().endsWith(".webm")) {
+            File inputFile = new File(inputFileString);
+
+            File outputFile = new File(encodingFolderPath.resolve(
+                FilenameUtils.removeExtension(inputFileString) + ".webm"
+            ).toAbsolutePath().toString());
+
+            String outputFileString = String.valueOf(outputFile);
+
+            builder.directory(inputFile.getParentFile());
+
+            List<String> cmd = new ArrayList<>(List.of(
                     getCWebMPath(),
-                    "-i"
-            };
+                    "-i",
+                    inputFile.getName(),
+                    "-c:v",
+                    "libvpx-vp9",
+                    "-vf",
+                    "scale=1920:1080",
+                    "-an",
+                    "-y",
+                    outputFileString
+            ));
 
-            List<String> cmd = new ArrayList<>(List.of(defaultCmd));
-            ProcessBuilder builder = new ProcessBuilder();
-            List<Process> processes = new ArrayList<>();
-            List<String> videoExtensions = Arrays.asList(".mp4", ".avi", ".mov", ".webm", ".wmv"); // todo: webm 일때 바로 저장
-            String savedFilePath = basePath + encodingPath + File.separator + FilenameUtils.removeExtension(file.getOriginalFilename());
-            String savedFilePathString = basePath + encodingPath + File.separator + file.getOriginalFilename();
-            Path encodingFolderPath = Path.of(basePath, encodingPath);
-
-            if (videoExtensions.stream().noneMatch(file.getOriginalFilename().toLowerCase()::endsWith)) {
-                throw new BadRequestException(ErrorCode.INVALID_FILE);
-            }
-
-            try (BufferedInputStream input = new BufferedInputStream(file.getInputStream());
-                    BufferedOutputStream output = new BufferedOutputStream(Files.newOutputStream(Path.of(savedFilePathString)))
-            ) {
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = input.read(buffer)) != -1) {
-                    output.write(buffer, 0, bytesRead);
-                }
-            } catch (IOException e) {
-                log.error("Failed to save the file: {}", e.getMessage());
-            }
-
-            File f = new File(savedFilePathString);
-
-            String outputFile = encodingFolderPath.resolve(
-                    savedFilePath + ".webm"
-            ).toAbsolutePath().toString();
-
-            builder.directory(f.getParentFile());
-            cmd.add(f.getName());
-            cmd.add("-c:v");
-            cmd.add("libvpx-vp9"); // 비디오 인코딩(VP9 코덱)
-            cmd.add("-b:v"); // 비디오 비트레이트 설정, 파일 크기 조정 가능
-            cmd.add("1M"); // 1Mbps
-            cmd.add("-crf"); // VP9 코덱의 화질 설정
-            cmd.add("30"); // 값이 클수록 화질이 낮아짐
-//            cmd.add("-vf"); // 비디오의 높이
-//            cmd.add("scale=-1:2160"); // 720픽셀로 조정
-            cmd.add("-an"); // 오디오 제거
-            cmd.add("-y"); // 덮어쓰기
-            cmd.add(outputFile);
             builder.command(cmd);
-            // todo: 업로드 가능한 파일 수는 1개여서 다른 태스크를 넣을 때 진행중이던 프로세스(쓰레드) 중지하고 클리어 한다음에 재 시작 필요
+
             try {
                 log.info("BUILDER: {}", builder.directory());
                 log.info("COMMAND: {}", builder.command());
@@ -105,12 +123,31 @@ public class EncodingService {
             }
 
             for (Process prc : processes) {
+                status = true;
                 try {
-                    // 프로세스 출력 및 에러 스트림을 읽기 위한 스레드 생성
+                    /* 프로세스 출력 및 에러 스트림을 읽기 위한 스레드 */
                     Thread outputThread = new Thread(() -> {
                         try (BufferedReader outputReader = new BufferedReader(new InputStreamReader(prc.getErrorStream()))) {
                             String outputLine;
-                            while ((outputLine = outputReader.readLine()) != null) {
+                            String duration = null;
+                            String time;
+                            while ((outputLine = outputReader.readLine()) != null && status) {
+                                /* 인코딩 할 총 시간 */
+                                if (outputLine.contains("Duration: ")) {
+                                    String remaining = outputLine.substring(outputLine.indexOf("Duration: ") + "Duration: ".length()).trim();
+                                    duration = remaining.substring(0, 11);
+                                    messagingTemplate.convertAndSend("/sub/message/duration", duration);
+                                }
+
+                                /* 인코딩 진행 중인 구간 */
+                                if (duration != null) {
+                                    if (outputLine.contains("time=") && !outputLine.contains("time=N/A")) {
+                                        String remaining = outputLine.substring(outputLine.indexOf("time=") + "time=".length()).trim();
+                                        time = remaining.substring(0, 11);
+                                        messagingTemplate.convertAndSend("/sub/message/time", time);
+                                    }
+                                }
+
                                 log.info("FFmpeg Output: {}", outputLine);
                             }
                         } catch (IOException e) {
@@ -118,7 +155,7 @@ public class EncodingService {
                         }
                     });
 
-                    // 출력 읽기 스레드 시작
+                    /* 출력 읽기 스레드 시작 */
                     outputThread.start();
                     int exitCode = prc.waitFor();
                     log.info("FFmpeg Exit Code: {}", exitCode);
@@ -128,27 +165,51 @@ public class EncodingService {
                 }
             }
             /* 변환 완료 된 파일 삭제 */
-            f.delete();
-
-            return CompletableFuture.completedFuture(file.getOriginalFilename());
+            inputFile.delete();
         }
 
-        /**
-         * FFmpeg WebM 변환 도구를 사용하여 비디오 변환
-         */
-        public String getCWebMPath() {
-            Path binaryPaths = Paths.get(
-                    basePath,
-                    "binary",
-                    IS_UNIX ? "linux" :
-                            IS_MAC ? "mac" :
-                                    IS_WINDOWS ? "windows" : "",
-                    "bin",
-                    "ffmpeg");
-            if (IS_UNIX || IS_WINDOWS || IS_MAC) {
-                return binaryPaths.toAbsolutePath().toString();
-            } else {
-                return null;
-            }
+        status = false;
+
+        return CompletableFuture.completedFuture(file.getOriginalFilename());
+    }
+
+    /**
+     * FFmpeg WebM 변환 도구를 사용하여 비디오 변환
+     */
+    public String getCWebMPath() {
+        Path binaryPaths = Paths.get(
+                basePath,
+                "binary",
+                IS_UNIX ? "linux" :
+                        IS_MAC ? "mac" :
+                                IS_WINDOWS ? "windows" : "",
+                "bin",
+                "ffmpeg");
+        if (IS_UNIX || IS_WINDOWS || IS_MAC) {
+            return binaryPaths.toAbsolutePath().toString();
+        } else {
+            return null;
         }
     }
+
+    /**
+     * 모든 프로세스 중지
+     */
+    public void stopAllProcesses() {
+        if (!processes.isEmpty()) {
+            for (Process process : processes) {
+                process.destroy();
+            }
+
+            processes.clear();
+            status = false;
+        }
+
+        try {
+            File encodingFolder = new File(basePath + File.separator + encodingPath);
+            FileUtils.cleanDirectory(encodingFolder); //하위 폴더와 파일 모두 삭제
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
